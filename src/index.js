883dc33384bb18d9a678744d252c85214f42fab6
James Rynn Berry <jb5260@nyu.edu>
2021-06-15 12:42:36 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById("create-task-form")
+  form.addEventListener('submit', e => {
+  let words = form['new-task-description'].value
+  let tasker = document.getElementById("tasks")
+  let elemental = document.createElement("li")
+  elemental.textContent = words,
+  tasker.appendChild(elemental),
+  e.preventDefault()
+  }
+  )
+
+})
+
+// let x = document.createElement("task");
+// x.setAttribute("text", "task");
+
+// Take the text we put in location #a, put it in location #b,
+// which is our #list. 
